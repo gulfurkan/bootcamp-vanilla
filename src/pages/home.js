e9dcc2519951fe52cd65bfebe0e98796e0013cc7
gulfurkan <furkangul0519@gmail.com>
2021-09-12 15:18:44 +0300

    Removing unused imports

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
 import NoteContext from '../context/NoteContext.js'
 import {NoteButton} from '../constants/styles'
 
